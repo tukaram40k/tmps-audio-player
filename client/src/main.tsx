bbc16354aeed7732be82acddc8c79161e70bc82c
tukaram40k <tukaram40k@gmail.com>
2025-11-01 14:27:25 +0200

    set up client

---
+++ client/src/main.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route} from "react-router";
+import App from './pages/App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+    </Routes>
+  </BrowserRouter>,
+)
